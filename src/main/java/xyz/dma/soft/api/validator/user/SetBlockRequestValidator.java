b0f17827c9a0aa3851750604b637efb282896dce
maksim.drobyshev <maks5299@yandex.ru>
2020-05-24 01:43:06 +0700

    Fix blocking

--- src/main/java/xyz/dma/soft/api/validator/user/SetBlockRequestValidator.java
+++ src/main/java/xyz/dma/soft/api/validator/user/SetBlockRequestValidator.java
@@ -29,7 +29,7 @@ public class SetBlockRequestValidator extends ARequestValidator<SetBlockRequest>
                     .validate(it -> notNull(it.getBlocked()), "blocked")
                     .chain()
                         .validate(it -> notNull(it.getUserId()), "userId")
-                        .validate(it -> !it.getUserId().equals(sessionInfo.getUserId()) ? ConstraintType.INVALID : null, "userId");
+                        .validate(it -> it.getUserId().equals(sessionInfo.getUserId()) ? ConstraintType.INVALID : null, "userId");
         return contextBuilder.build();
     }
 }
